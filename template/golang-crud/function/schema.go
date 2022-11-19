5773884646fc2cdfc448c5bb1c0ef422ef95ca45
Simon Emms <simon@simonemms.com>
2022-11-19 17:21:35 +0000

    feat(golang-crud): configure validation

--- template/golang-crud/function/schema.go
+++ template/golang-crud/function/schema.go
@@ -1,9 +1,11 @@
 package function
 
+import "github.com/go-playground/validator/v10"
+
 // Define the MongoDB schema
 type Schema struct {
-	Name         string `json:"name" bson:"name"`
-	EmailAddress string `json:"emailAddress" bson:"emailAddress"`
+	Name         string `json:"name" bson:"name" validate:"required"`
+	EmailAddress string `json:"emailAddress" bson:"emailAddress" validate:"required,email"`
 }
 
 // Set the database collection name
@@ -15,3 +17,11 @@ func (model *Schema) CollectionName() string {
 func (model *Schema) Creating() error {
 	return nil
 }
+
+// Custom validation tags https://pkg.go.dev/github.com/go-playground/validator/v10#hdr-Custom_Validation_Functions
+// Message uses universal translator https://pkg.go.dev/github.com/go-playground/universal-translator
+// {0} is the field name, {1} is the value
+var Validation = map[string]struct {
+	Message   string
+	Validator validator.Func
+}{}
--- template/golang-crud/go.mod
+++ template/golang-crud/go.mod
@@ -5,6 +5,9 @@ go 1.19
 require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/gin-gonic/gin v1.8.1
+	github.com/go-playground/locales v0.14.0
+	github.com/go-playground/universal-translator v0.18.0
+	github.com/go-playground/validator/v10 v10.11.1
 	github.com/google/uuid v1.3.0
 	github.com/kamva/mgm/v3 v3.5.0
 	github.com/mrsimonemms/openfaas-templates/template/golang-crud/function v0.0.0-00010101000000-000000000000
@@ -14,9 +17,6 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.14.0 // indirect
-	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
