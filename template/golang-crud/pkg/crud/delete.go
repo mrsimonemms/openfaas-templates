e59dd11ca558bd6a0b80d377d0845f24dd7ba461
Simon Emms <simon@simonemms.com>
2022-11-19 21:21:17 +0000

    feat(golang-crud): do the delete endpoint

--- template/golang-crud/pkg/crud/delete.go
+++ template/golang-crud/pkg/crud/delete.go
@@ -1,14 +1,36 @@
 package crud
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kamva/mgm/v3"
+	"github.com/mrsimonemms/openfaas-templates/template/golang-crud/pkg/common"
+	"github.com/mrsimonemms/openfaas-templates/template/golang-crud/pkg/function"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (h handler) Delete(c *gin.Context) {
-	fmt.Println("delete")
+	coll := mgm.Coll(&function.Schema{})
 
-	c.Status(http.StatusOK)
+	result, err := coll.DeleteOne(mgm.Ctx(), bson.D{
+		{
+			Key:   "id",
+			Value: c.Param("id"),
+		},
+	})
+	if err != nil {
+		common.ErrorHandler(c, http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	if result.DeletedCount == 0 {
+		common.ErrorHandler(c, http.StatusNotFound)
+		return
+	}
+
+	// Send no content with JSON headers
+	c.JSON(http.StatusNoContent, struct{}{})
 }
