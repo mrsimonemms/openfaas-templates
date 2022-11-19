97c2b4fc2d82d6c3c925ff72ce74597ab3c0ff76
Simon Emms <simon@simonemms.com>
2022-11-19 22:26:58 +0000

    feat(golang-crud): do the getOne endpoint

--- template/golang-crud/pkg/crud/get_one.go
+++ template/golang-crud/pkg/crud/get_one.go
@@ -1,15 +1,40 @@
 package crud
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kamva/mgm/v3"
+	"github.com/mrsimonemms/openfaas-templates/template/golang-crud/pkg/common"
+	"github.com/mrsimonemms/openfaas-templates/template/golang-crud/pkg/function"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func (h handler) GetOne(c *gin.Context) {
-	id := c.Param("id")
-	fmt.Println("get one", id)
+	data := function.Schema{}
+	coll := mgm.Coll(&data)
+	filter := bson.D{
+		{
+			Key:   "id",
+			Value: c.Param("id"),
+		},
+	}
 
-	c.Status(http.StatusOK)
+	if err := coll.FindOne(mgm.Ctx(), filter).Decode(&data); err != nil {
+		status := http.StatusServiceUnavailable
+		var msg []gin.H
+		if err == mongo.ErrNoDocuments {
+			status = http.StatusNotFound
+		} else {
+			msg = append(msg, gin.H{
+				"message": err.Error(),
+			})
+		}
+
+		common.ErrorHandler(c, status, msg...)
+		return
+	}
+
+	c.JSON(http.StatusOK, data)
 }
